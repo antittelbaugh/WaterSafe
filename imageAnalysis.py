9c50b829a6f8cbe4a40b776b78461fa6e45eb6a9
antittelbaugh <85364208+antittelbaugh@users.noreply.github.com>
2024-03-11 14:07:45 -0700

    Add files via upload

---
+++ imageAnalysis.py
@@ -0,0 +1,24 @@
+from skimage.filters import threshold_mean
+from skimage import io
+from skimage.color import rgb2gray, label2rgb
+from skimage.measure import label, regionprops_table
+from skimage.morphology import closing
+import matplotlib.pyplot as plt
+import pandas as pd
+
+img=io.imread('MP_PA_1.jpg')
+img = rgb2gray(img)
+thresh=threshold_mean(img)
+bw=closing(img > thresh)
+label_image = label(bw)
+labeled_img = label2rgb(label_image,image=img,bg_label=0)
+io.imshow(labeled_img)
+props = regionprops_table(label_image,properties = ['label','feret_diameter_max'])
+df = pd.DataFrame(props)
+df['feret_diameter_max'] = df['feret_diameter_max'] * .58
+df = df[df['feret_diameter_max'] >= 17.24]
+hist = df['feret_diameter_max'].hist(bins=10,edgecolor='black')
+plt.xlabel('Size (um)')
+plt.ylabel('Frequency')
+plt.title('Size Distribution')
+plt.show()
